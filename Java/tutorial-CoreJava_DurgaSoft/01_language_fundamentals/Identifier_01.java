a1941e68b8d6f10901d97f621a08dfd68192bf93
Rajesh Sinha <rajeshsinha1997@gmail.com>
2024-10-14 23:52:27 +0530

    14_10_2024 - Core Java

---
+++ Java/tutorial-CoreJava_DurgaSoft/01_language_fundamentals/Identifier_01.java
@@ -0,0 +1,15 @@
+public class Identifier_01 {
+    public static void main(String[] args) {
+        int x = 10;
+    }
+}
+
+/*
+ * There are a total of 5 identifiers in the above program
+ * 
+ * 1. Test - Name of the class
+ * 2. main - Name of the method
+ * 3. String - Name of a pre defined class
+ * 4. args - Name of a variable
+ * 5. x - Name of a variable
+ */
---
+++ Java/tutorial-CoreJava_DurgaSoft/01_language_fundamentals/Identifier_02.java
@@ -0,0 +1,9 @@
+public class Identifier_02 {
+    public static void main(String[] args) {
+        int String = 888;
+        int Runnable = 999;
+
+        System.out.println(String);
+        System.out.println(Runnable);
+    }
+}
