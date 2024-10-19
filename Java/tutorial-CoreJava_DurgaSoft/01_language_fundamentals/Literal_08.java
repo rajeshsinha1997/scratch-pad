955d5556dcfd191fc71996b125f2e0380b504e80
Rajesh Sinha <rajeshsinha1997@gmail.com>
2024-10-20 00:19:28 +0530

    19_10_2024 - Core Java

---
+++ Java/tutorial-CoreJava_DurgaSoft/01_language_fundamentals/Literal_08.java
@@ -0,0 +1,7 @@
+public class Literal_08 {
+    public static void main(String[] args) {
+        int binaryLiteral = 0B0101;
+
+        System.out.println("Binary Literal: " + binaryLiteral);
+    }
+}
