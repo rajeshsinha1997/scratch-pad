333454aa6c082230d0775bd2953532409f208d01
Rajesh Sinha <rajeshsinha1997@gmail.com>
2024-10-28 13:25:44 +0530

    28_10_2024 - Core Java

---
+++ Java/core_java_durgasoft/01_language_fundamentals/MM_01.java
@@ -0,0 +1,10 @@
+public class MM_01 {
+
+    public static void main(int[] args) {
+        System.out.println("Overloaded Main Method");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Normal Main Method");
+    }
+}
---
+++ Java/core_java_durgasoft/01_language_fundamentals/MM_02.java
@@ -0,0 +1,11 @@
+// parent class
+public class MM_02 {
+    public static void main(String[] args) {
+        System.out.println("Parent Class Main Method");
+    }
+}
+
+// child class
+class C_MM_02 extends MM_02 {
+
+}
